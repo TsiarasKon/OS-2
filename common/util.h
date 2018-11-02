fc0073c37d843382a0fe92ef2e824fff5d5572ef
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2018-11-02 23:54:28 +0200

    Fork()s and execs from root node
    - lacks pipes

--- common/util.h
+++ common/util.h
@@ -6,6 +6,7 @@ enum ErrorCodes {
     EC_ARG,      // Invalid command line arguments
     EC_FILE,     // Failed to open/read file
     EC_FORK,     // Error while forking
+    EC_EXEC,     // Failed to exec
     EC_PIPE,     // Error related to pipes
     EC_MEM,      // Failed to allocate memory
     EC_UNKNOWN   // An unexpected error
--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <getopt.h>
-#include "util.h"
+#include "common/util.h"
 
 int main(int argc, char *argv[]) {
     const char argErrorMsg[] = "Invalid arguments. Please run \"$ ./myfind -h Height -d Datafile -p Pattern [-s]\"\n";
@@ -11,7 +13,7 @@ int main(int argc, char *argv[]) {
         printf("%s", argErrorMsg);
         return EC_ARG;
     }
-    int height = -1;
+    int height = 0;
     char *strtolEndptr = NULL;
     char *datafile = NULL;
     char *pattern = NULL;
@@ -23,8 +25,7 @@ int main(int argc, char *argv[]) {
                 height = (int) strtol(optarg, &strtolEndptr, 10);
                 if (*strtolEndptr != '\0' || height < 1 || height > 5) {
                     printf("Invalid arguments: Height must be an integer between 1 and 5 (inclusive).\n");
-                    free(datafile);
-                    free(pattern);
+                    free(datafile); free(pattern);
                     return EC_ARG;
                 }
                 break;
@@ -41,21 +42,51 @@ int main(int argc, char *argv[]) {
                 break;
             default:
                 printf("%s", argErrorMsg);
-                free(datafile);
-                free(pattern);
+                free(datafile); free(pattern);
                 return EC_ARG;
         }
     }
-    if (height == -1 || datafile == NULL || pattern == NULL) {
+    if (height == 0 || datafile == NULL || pattern == NULL) {
         printf("%s", argErrorMsg);
-        free(datafile);
-        free(pattern);
+        free(datafile); free(pattern);
         return EC_ARG;
     }
 
-    printf("%d %s %s %d\n", height, datafile, pattern, skew);
+    if ( access(datafile, R_OK) == -1 ) {
+        fprintf(stderr, "Cannot read from '%s'\n", datafile);
+        free(datafile); free(pattern);
+        return EC_FILE;
+    }
 
+    // TODO create pipe
+    int smRootPid = fork();
+    if (smRootPid < 0) {
+        perror("fork");
+        free(datafile); free(pattern);
+        return EC_FORK;
+    } else if (smRootPid == 0) {
+        execl("../splitter_merger", "splitter_merger", datafile, 0, 1, pattern, height, (char *) NULL);
+        // this code will run only if exec fails:
+        perror("execl");
+        free(datafile); free(pattern);
+        return EC_EXEC;
+    }
     free(datafile);
     free(pattern);
+
+    // TODO: stuff
+
+    // TODO create pipe
+    int sortPid = fork();
+    if (sortPid < 0) {
+        perror("fork");
+        // TODO: frees?
+        return EC_FORK;
+    } else if (sortPid == 0) {
+        execl("../sorter", "sorter", /* search results, */ (char *) NULL);
+        perror("execl");
+        return EC_EXEC;
+    }
+
     return 0;
 }
