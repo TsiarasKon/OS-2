b5b1d46c3a09579dff7be445ec1a19b1f8db8fcd
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2018-11-06 23:48:58 +0200

    Splitter-Merger getting data from Searchers' pipes

--- splitter_merger.c
+++ splitter_merger.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <wait.h>
+#include <poll.h>
 #include "common/util.h"
 #include "record.h"
 
@@ -40,12 +42,16 @@ int main(int argc, char *argv[]) {
 
     int fd1[2];
     pipe(fd1);
-    pid_t pid1 = fork();
     int rangeSplit = (rangeStart + rangeEnd) / 2;         // TODO: assuming skew == 0
     char rangeStartStr[MAX_NUM_STRING_SIZE];
     char rangeEndStr[MAX_NUM_STRING_SIZE];
     char rootPidStr[MAX_NUM_STRING_SIZE];
     sprintf(rootPidStr, "%d", rootPid);
+    pid_t pid1 = fork();
+    if (pid1 < 0) {
+        perror("[Splitter-Merger] fork");
+        return EC_FORK;
+    }
     if (pid1 == 0) {         // first child
         dup2(fd1[WRITE_END], STDOUT_FILENO);
         close(fd1[READ_END]);
@@ -70,6 +76,10 @@ int main(int argc, char *argv[]) {
     int fd2[2];
     pipe(fd2);
     pid_t pid2 = fork();
+    if (pid2 < 0) {
+        perror("[Splitter-Merger] fork");
+        return EC_FORK;
+    }
     if (pid2 == 0) {         // second child
         close(fd1[READ_END]);
         dup2(fd2[WRITE_END], STDOUT_FILENO);
@@ -86,13 +96,65 @@ int main(int argc, char *argv[]) {
             char skewStr[MAX_NUM_STRING_SIZE];
             sprintf(skewStr, "%d", skew);
             execl("./splitter_merger", "splitter_merger", datafile, rangeStartStr,
-                  rangeEndStr, pattern, height - 1, skew, rootPidStr, (char *) NULL);
+                  rangeEndStr, pattern, heightStr, skewStr, rootPidStr, (char *) NULL);
         }
         perror("[Splitter-Merger] execl");
         return EC_EXEC;
     }
     close(fd2[WRITE_END]);
 
+    struct pollfd pollfd[2];
+    pollfd[0].fd = fd1[READ_END];
+    pollfd[1].fd = fd2[READ_END];
+    pollfd[0].events = pollfd[1].events = POLLIN;
+    pollfd[0].revents = pollfd[1].revents = 0;
 
+    bool child_completed[2] = {false, false};
+    int nextStructIndicator;
+    Record currRecord;
+    Statistics currStats;
+    while (!child_completed[0] || !child_completed[1]) {
+        if (poll(pollfd, (nfds_t) 2, -1) < 0) {
+            perror("[Splitter-Merger] poll");
+            return EC_PIPE;
+        }
+        for (int i = 0; i < 2; i++) {       // poll both children
+            if (pollfd[i].revents & POLLIN && !child_completed[i]) {       // we can read from child i
+                if (read(pollfd[i].fd, &nextStructIndicator, sizeof(int)) < 0) {
+                    perror("[Splitter-Merger] Error reading from pipe");
+                    return EC_PIPE;
+                }
+                if (nextStructIndicator == 0) {     // Next struct is a Record
+                    if (read(pollfd[i].fd, &currRecord, sizeof(Record)) < 0) {
+                        perror("[Splitter-Merger] Error reading from pipe");
+                        return EC_PIPE;
+                    }
+//                    fwrite(&nextStructIndicator, sizeof(int), 1, stdout);
+//                    fwrite(&currRecord, sizeof(Record), 1, stdout);
+                    printRecord(currRecord);        /// DEBUG
+                } else if (nextStructIndicator == 1) {        // Next struct is Statistics
+                    if (read(pollfd[i].fd, &currStats, sizeof(Statistics)) < 0) {
+                        perror("[Splitter-Merger] Error reading from pipe");
+                        return EC_PIPE;
+                    }
+                    child_completed[i] = true;
+                } else {        // should never get here
+                    fprintf(stderr, "[Splitter-Merger] Received invalid data from pipe.\n");
+                    return EC_INVALID;
+                }
+            }
+        }
+    }
 
+    nextStructIndicator = 1;
+    // TODO: generate and write sm stats
+//    fwrite(&nextStructIndicator, sizeof(int), 1, stdout);
+//    fwrite(&stats, sizeof(Statistics), 1, stdout);
+
+    close(fd1[READ_END]);
+    close(fd2[READ_END]);
+    // wait for both children:
+    wait(NULL);
+    wait(NULL);
+    return EC_OK;
 }
